ea40766b04994f4c8e8a663b1cd4cbbe1c63bb1f
popomama <wei_xia@hotmail.com>
2015-05-24 17:07:00 -0400

    add another comment

--- Try426/Global.asax.cs
+++ Try426/Global.asax.cs
@@ -14,11 +14,13 @@ namespace Try426
     {
         protected void Application_Start()
         {
+            //want to see if this is an issue
             Database.SetInitializer(new BookDBInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
